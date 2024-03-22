2b6eae2993672c7dc4938ca61d221113391620b0
suchir-cyber <suchirpandula@gmail.com>
2024-03-23 01:47:33 +0530

    added authentication

--- blog_web/blog/views.py
+++ blog_web/blog/views.py
@@ -4,27 +4,6 @@ from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'name' : 'suchir',
-        'title' : 'Blog  1',
-        'content' : 'first post',
-        'date_posted' : 'March 22,2024'
-    },
-    {
-        'name' : 'kumar',
-        'title' : 'Blog  2',
-        'content' : 'second post',
-        'date_posted' : 'March 23,2024'
-    },
-    {
-        'name' : 'pavan',
-        'title' : 'Blog  3',
-        'content' : 'third post',
-        'date_posted' : 'March 24,2024'
-    }
-]
-
 def home(request):
     context = {
         'posts' : Post.objects.all(),
--- blog_web/blog_web/settings.py
+++ blog_web/blog_web/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
