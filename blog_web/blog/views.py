7896e9e038ac530b1339bb3415c0f598c1d00db8
suchir-cyber <suchirpandula@gmail.com>
2024-03-22 17:25:31 +0530

    done inheritance

--- blog_web/blog/views.py
+++ blog_web/blog/views.py
@@ -2,8 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+posts = [
+    {
+        'name' : 'suchir',
+        'title' : 'Blog  1',
+        'content' : 'first post',
+        'date_posted' : 'March 22,2024'
+    },
+     {
+        'name' : 'kumar',
+        'title' : 'Blog  2',
+        'content' : 'second post',
+        'date_posted' : 'March 23,2024'
+    }
+]
+
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts' : posts,
+        'title' : 'Home'
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html',{'title' : 'About'})
