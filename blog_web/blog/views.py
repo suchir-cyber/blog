be0159bcac3caaabb8b298c6b2ed4cbfccdf4663
suchir-cyber <suchirpandula@gmail.com>
2024-03-23 00:55:20 +0530

    updated Post model

--- blog_web/blog/views.py
+++ blog_web/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
@@ -10,17 +11,23 @@ posts = [
         'content' : 'first post',
         'date_posted' : 'March 22,2024'
     },
-     {
+    {
         'name' : 'kumar',
         'title' : 'Blog  2',
         'content' : 'second post',
         'date_posted' : 'March 23,2024'
+    },
+    {
+        'name' : 'pavan',
+        'title' : 'Blog  3',
+        'content' : 'third post',
+        'date_posted' : 'March 24,2024'
     }
 ]
 
 def home(request):
     context = {
-        'posts' : posts,
+        'posts' : Post.objects.all(),
         'title' : 'Home'
     }
     return render(request,'blog/home.html',context)
